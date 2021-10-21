01667c193034ee1c97dcd45e8c1ffadcb99c27de
Ramya23k <92727946+Ramya23k@users.noreply.github.com>
2021-10-21 15:18:55 +0530

    Add files via upload

---
+++ ex4.rb
@@ -0,0 +1,7 @@
+
+puts "There are 100 cars available."
+puts "There are only 30 drivers available."
+puts "There will be 70 empty cars today."
+puts "We can transport 120 people today."
+puts "We have 90 to carpool today."
+puts "We need to put about 3 in each car."
